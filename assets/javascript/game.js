9a0f4d1a70cc013d635a2c177015ee68a12134c4
jlcampbell16 <45213854+Jlcampbell16@users.noreply.github.com>
2019-03-05 21:18:23 -0700

    elements on HTML, crystalvalue duplicating per click

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,67 +1,66 @@
 // GLOBAL VARIABLES
-var userScore = 0;
-var targetScore = 0;
-var wins = 0;
-var losses = 0;
+var userScore;
+var targetNumber;
+var wins;
+var losses;
+var counter = 0
 
 
-//FUNTIONS
-var targetNumber = 50;
-$("number-to-guess").text(targetNumber);
+// array for 4 crystals and their random values ( 1 - 12)
+// 
 
-//create on-click event for the crystals button
+//-----FUNCTIONS-----
+function initializeScreen() {
+    //generate a random number for the target number and display on screen
+    var targetNumber = Math.floor(Math.random() * 120) + 19;
+    $("#number-to-guess").text(targetNumber);
 
-var counter = 0;
-var numberOptions = [10, 5, 3, 7];
-for (var i = 0; i < numberOptions.length; i++) {
-    var imageCrystal = $("<img>");
-    imageCrystal.addClass("crystal-image");
-    imageCrystal.attr("src", "https://b.kisscc0.com/20180717/we/kisscc0-rubygems-gemstone-emerald-sapphire-round-ruby-5b4eb92c969420.9777605815318858686168.jpg");
-    imageCrystal.attr("data-crystalvalue", numberOptions[i]);
-    $("#crystals").append(imageCrystal);
+    userScore = 0; // or ""?
+    wins = 0;
+    losses = 0;
+}
+
+function resetGame() {
+    var targetNumber = Math.floor(Math.random() * 120) + 19;
+    $("#number-to-guess").text(targetNumber);
+    userScore = 0
+}
+
+var valueOptions = Math.floor(Math.random() * 12) + 1;
+for (var i = 0; i < valueOptions.length; i++) {  //is valueOptions.length correct? that would run 12 times
 
 }
-var increment = numberOptions[Math.round(Math.random())];
 
-$(".crystal-image").on("click", function () {
-    var crystalValue = ($(this).attr("data-crystalvalue"));
-    crystalValue = parseInt(crystalValue);
-    counter += increment;
-        alert("New score: " + counter);
+
+//-----PROCESS-----
+
+$(document).ready(function () {
+
+initializeScreen();
+
+//when a crystal is clicked
+$(".crystals").on("click", function () {
+    console.log("crystal value:" + valueOptions);
+
+
+
+    
+    $("#your-guess").text(valueOptions)
     if (counter === targetNumber) {
-        alert ("you win!");
+        wins++
+        alert("You Win!");
     } else if (counter > targetNumber) {
-        alert ("you lose!");
+        losses++
+        alert("Try again!");
     }
 
 });
 
 
+var userGuess = 0;
+$("#user-guess").text(userGuess)
 
-    // create random numbers - for each crystal with a max & min value. (USE RETURN as the VALUE)
 
-    // create the crystals - hard code four crytasl and attach a random value to each one
-    // random value for the target score (.text, .html, or append it) - reset each game
-    // clear out the varuables - wins, losses, and your score
-
-    //need a way to add to the score. is your score less than or grater than target score - one function 
-        //click event with one class to cover all four crystals
-        //determine the value for each crystal (use .this to determine the value for what was clicked on)
-
-    //take crystal just clicked on and add it to current score (var score = crystal value) 
-
-    //function to reset entire game
-    //loss condition - loss count++
-    //win condition - 
-
-    // create an initial function 
-        //wins = 0, losses = 0, target score = random, user score = 0
-
-    // create a reset function 
-        //target score = random, user score = 0
-
-//PROCESS
-
-//DISPLAY
-    //use jQuery to create elements in HTML
+resetGame ();
+});
 
