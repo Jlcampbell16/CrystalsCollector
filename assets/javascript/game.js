c67c5f938e9f1b64cbcf85f4367e4e984546969c
jlcampbell16 <45213854+Jlcampbell16@users.noreply.github.com>
2019-03-06 18:22:54 -0700

    javascript updates post tutoring session

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,66 +1,74 @@
-// GLOBAL VARIABLES
-var userScore;
-var targetNumber;
-var wins;
-var losses;
-var counter = 0
-
-
-// array for 4 crystals and their random values ( 1 - 12)
-// 
-
-//-----FUNCTIONS-----
-function initializeScreen() {
-    //generate a random number for the target number and display on screen
-    var targetNumber = Math.floor(Math.random() * 120) + 19;
-    $("#number-to-guess").text(targetNumber);
-
-    userScore = 0; // or ""?
-    wins = 0;
-    losses = 0;
-}
-
-function resetGame() {
-    var targetNumber = Math.floor(Math.random() * 120) + 19;
-    $("#number-to-guess").text(targetNumber);
-    userScore = 0
-}
-
-var valueOptions = Math.floor(Math.random() * 12) + 1;
-for (var i = 0; i < valueOptions.length; i++) {  //is valueOptions.length correct? that would run 12 times
-
-}
-
-
-//-----PROCESS-----
-
 $(document).ready(function () {
 
-initializeScreen();
 
-//when a crystal is clicked
-$(".crystals").on("click", function () {
-    console.log("crystal value:" + valueOptions);
+    // GLOBAL VARIABLES
+    var userGuess = 0;
+    var targetNumber;
+    var wins = 0;
+    var losses = 0;
+    var counter = 0;
+    var crystal1 = Math.floor(Math.random() * 12) + 1;
+    var crystal2 = Math.floor(Math.random() * 12) + 1;
+    var crystal3 = Math.floor(Math.random() * 12) + 1;
+    var crystal4 = Math.floor(Math.random() * 12) + 1;
 
+    // array for 4 crystals and their random values ( 1 - 12)
+    // 
 
+    //-----FUNCTIONS-----
+    function initializeScreen() {
+        //generate a random number for the target number and display on screen
+        var targetNumber = Math.floor(Math.random() * 120) + 19;
+        $("#number-to-guess").text(targetNumber);
 
-    
-    $("#your-guess").text(valueOptions)
-    if (counter === targetNumber) {
-        wins++
-        alert("You Win!");
-    } else if (counter > targetNumber) {
-        losses++
-        alert("Try again!");
+        userScore = 0; // or ""?
+        wins = 0;
+        losses = 0;
     }
 
-});
+    function resetGame() {
+        var targetNumber = Math.floor(Math.random() * 120) + 19;
+        $("#number-to-guess").text(targetNumber);
+        // userScore = 0;
+        counter = 0;
+        crystal1 = Math.floor(Math.random() * 12) + 1;
+        crystal2 = Math.floor(Math.random() * 12) + 1;
+        crystal3 = Math.floor(Math.random() * 12) + 1;
+        crystal4 = Math.floor(Math.random() * 12) + 1;
+        $("#crystal1").attr("value", crystal1);
+        $("#crystal2").attr("value", crystal2);
+        $("#crystal3").attr("value", crystal3);
+        $("#crystal4").attr("value", crystal4);
+    }
 
 
-var userGuess = 0;
-$("#user-guess").text(userGuess)
+
+    //-----PROCESS-----
 
 
-resetGame ();
+    initializeScreen();
+
+    //when a crystal is clicked
+    $(".crystals").on("click", function () {
+        counter = counter + parseInt($(this).attr("value"));
+        $("#your-guess").text(counter) // these values need to add together
+        if (counter === targetNumber) {
+            wins++
+            $("#winsText").text(wins)
+            resetGame();
+        } else if (counter > targetNumber) {
+            losses++
+            alert("Try again!");
+            $("#winsText").text(wins)
+            resetGame();
+        }
+
+    });
+
+
+
+
+
+    resetGame();
 });
 
